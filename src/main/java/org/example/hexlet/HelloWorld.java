bad5ba737a383c982957e53d94b54da0a225a709
aemeth <aemeth.lvx@gmail.com>
2024-01-10 13:48:05 +0300

    Add `hello` route

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -4,12 +4,17 @@ import io.javalin.Javalin;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        // Создаем приложение
+
         var app = Javalin.create(config -> {
             config.plugins.enableDevLogging();
         });
-        // Описываем, что загрузится по адресу /
+
         app.get("/", ctx -> ctx.result("Hello World"));
+        app.get("/hello", ctx -> {
+            var name = ctx.queryParamAsClass("name", String.class).getOrDefault("World");
+            ctx.result("Hello " + name + "!");
+        });
+
         app.start(7070); // Стартуем веб-сервер
     }
 }
